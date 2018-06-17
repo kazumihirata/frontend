76ca0b6b75d67c4df93ef64bea274f7a91f9f741
kazumi hirata <hiratakazumi2020@gmail.com>
2018-06-17 07:10:19 +0700

    add fullpagejs

---
+++ fullpagejs_test/my.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['dog', 'cat', 'mouse', 'hamutaro', 'rabit', 'fox'],
+        sectionsColor: ['#7fffff', '#7fffbf', '#7fff7f', '#bfff7f', '#ffff7f', '#ffbf7f'],
+        menu: '#menu',
+        continuousVertical: false
+    });
+})
