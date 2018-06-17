09b638417e51f34b4f532cd9a3033985772fe8c7
kazumi hirata <hiratakazumi2020@gmail.com>
2018-06-17 15:54:02 +0700

    add media query

--- fullpagejs_test/my.js
+++ fullpagejs_test/my.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         anchors: ['dog', 'cat', 'mouse', 'hamutaro', 'rabit', 'fox'],
         sectionsColor: ['#7fffff', '#7fffbf', '#7fff7f', '#bfff7f', '#ffff7f', '#ffbf7f'],
         menu: '#menu',
-        continuousVertical: false
+        continuousVertical: false,
+        navigation: true
     });
 })
